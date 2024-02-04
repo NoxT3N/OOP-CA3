fbcf31680415e1e8435d9e7a876f4898bcaf6fe2
Felix <145358483+NoxT3N@users.noreply.github.com>
2024-02-04 16:22:22 +0000

    added menu loop for Q1

--- src/CA3_Question1.java
+++ src/CA3_Question1.java
@@ -12,46 +12,48 @@ public class CA3_Question1
         Scanner keyboard = new Scanner(System.in);
         Stack<Integer> driveway = new Stack<>();
         Stack<Integer> street = new Stack<>();
-
+        int choice;
         driveway.push(1);
         driveway.push(2);
         driveway.push(3);
         driveway.push(4);
 
-        System.out.println("Cars in the driveway: "+ driveway.toString());
-        System.out.println("Enter car to add/remove");
-        int choice = keyboard.nextInt();
+        do{
+            System.out.println("Cars in the driveway: "+ driveway.toString());
+            System.out.println("Enter car to add/remove:\nEnter 0 to quit simulation");
+            choice = keyboard.nextInt();
 
-        if(choice >= 0){
-            driveway.push(choice);
-        }
-        else{
-            choice = choice * -1;
-
-            if(driveway.peek()== choice){
-                driveway.pop();
-                street.push(choice);
+            if(choice > 0){
+                driveway.push(choice);
                 System.out.println("Driveway: "+driveway.toString());
-                System.out.println("Street: "+street.toString());
             }
-            else{
-                while(driveway.peek()!= choice){
-                    street.push(driveway.peek());
+            else if(choice < 0) {
+                choice = choice * -1;
+
+                if (driveway.peek() == choice) {
                     driveway.pop();
-                }
-                System.out.println("Driveway: "+driveway.toString());
-                System.out.println("Street: "+street.toString());
+                    street.push(choice);
+                    System.out.println("Driveway: " + driveway.toString());
+                    System.out.println("Street: " + street.toString());
+                } else {
+                    while (driveway.peek() != choice) {
+                        street.push(driveway.peek());
+                        driveway.pop();
+                    }
+                    System.out.println("Driveway: " + driveway.toString());
+                    System.out.println("Street: " + street.toString());
 
-                driveway.pop();
+                    driveway.pop();
 
-                while(!street.isEmpty()){
-                    driveway.push(street.peek());
-                    street.pop();
+                    while (!street.isEmpty()) {
+                        driveway.push(street.peek());
+                        street.pop();
+                    }
+                    System.out.println("Driveway: " + driveway.toString());
+                    System.out.println("Street: " + street.toString());
                 }
-                System.out.println("Driveway: "+driveway.toString());
-                System.out.println("Street: "+street.toString());
             }
-        }
+        }while(choice !=0);
 
     }
 
